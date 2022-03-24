1194aba05ee0b76e867423531ef70cbbeec70483
CodeNode21 <mbednara@gmail.com>
2022-03-24 13:12:32 -0400

    fitting in the js file

--- tleonard.js
+++ tleonard.js
@@ -5,61 +5,56 @@ const port = 3001;
 const content = `
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Tommy Leonard</title>
-    <link href="" />
-    <link href="css/style.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="content">
-      <header class="hero">
-        <div class="image">
-          <img src="./assets/tommyBoi.png" alt="legendary" />
-        </div>
-        <div class="bio">Rapper | Singer | Producer</div>
-      </header>
-      <div class="video">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/J0oeEKTIppY"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </div>
-      <div class="links">
-        <div class="link apple">
-          <!-- <img src="assets/itunes.png" /> -->
-          <a href="https://music.apple.com/us/artist/tommy-boy/1547198156"
-            ><img src="./assets/Button-Apple.png" alt="apple music" />
-          </a>
-        </div>
-        <div class="link spotify">
-          <!-- <img src="assets/spotify.png"/> -->
-          <a
-            href="https://open.spotify.com/artist/39SZhrlotMDfYHfzKwyF2R?si=qMMZmHaJTPaMcb2FsO2DEA&dl_branch=1"
-            ><img src="./assets/Button-Spotify.png" alt="spotify" /></a
-          >
-        </div>
-        <div class="link youtube">
-          <!-- <img src="assets/youtube.png"/> -->
-          <a href="https://www.youtube.com/channel/UCWSXcd6mBV5bFX-W5m0TclQ"
-            ><img src="./assets/Buttons-YT.png" alt="youtube" /></a
-          >
-        </div>
-        <div class="link ig">
-          <!-- <img src="assets/instagram.png"/> -->
-          <a href="https://www.instagram.com/tommyleonardjr/"><img src="./assets/Buttons-IG.png" alt="instagram" /></a>
-        </div>
-      </div>
-      <footer class="footer">2022 Leonard Ent.</footer>
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Tommy Leonard</title>
+<link href="" />
+<link href="css/style.css" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="content">
+  <header class="hero">
+    <div class="hero-image">
+      <img src="./assets/headerTitle.png" alt="Tommy Leonard" />
+      <!-- <img src="./assets/crisp1.jpg" alt="legendary" /> -->
     </div>
-  </body>
+  </header>
+<div class="links">
+<div class="link apple">
+  <a href="https://music.apple.com/us/artist/tommy-boy/1547198156">
+    Apple Music
+  </a>
+</div>
+<div class="link spotify">
+  <a
+    href="https://open.spotify.com/artist/39SZhrlotMDfYHfzKwyF2R?si=qMMZmHaJTPaMcb2FsO2DEA&dl_branch=1"
+  >
+    Spotify
+  </a>
+</div>
+<div class="link youtube">
+  <a href="https://www.youtube.com/channel/UCWSXcd6mBV5bFX-W5m0TclQ"
+    >YouTube</a
+  >
+</div>
+<div class="link ig">
+  <a href="https://www.instagram.com/tommyleonardjr/">Instagram</a>
+</div>
+<div class="link twitter">
+  <a href="https://twitter.com/tommyleonardjr/">Twitter</a>
+</div>
+<div class="link tiktok">
+  <a href="https://www.tiktok.com/@tommyleonardjr/">TikTok</a>
+</div>
+</div>
+<footer class="footer">2022 Leonard Ent.</footer>
+</div>
+</body>
 </html>
 
 `;
