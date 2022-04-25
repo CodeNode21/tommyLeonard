391a2306dc8fd77e49a0c56c61a353d18f008e59
CodeNode21 <mbednara@gmail.com>
2022-04-25 13:42:37 -0400

    added meto robots follow index

--- tleonard.js
+++ tleonard.js
@@ -9,6 +9,8 @@ const content = `
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name='robots' content='index, follow'/>
+
 <title>Tommy Leonard</title>
 <link href="" />
 <link href="css/style.css" rel="stylesheet" />
